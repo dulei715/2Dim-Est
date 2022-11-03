78a1167657a5fa4d081a2b7ed786462a64f2e0f3
pisces <dulei715@126.com>
2022-11-03 17:09:52 +0800

    aaa

---
+++ src/main/java/ecnu/dll/construction/specialtools/GridLocalPrivacyTool.java
@@ -0,0 +1,15 @@
+package ecnu.dll.construction.specialtools;
+
+import cn.edu.ecnu.struct.point.TwoDimensionalIntegerPoint;
+
+public class GridLocalPrivacyTool {
+
+    public static Integer getGridSize(int sizeD) {
+        return sizeD * sizeD;
+    }
+
+
+    public static Integer getCrossCellQuantity(int sizeD, int sizeB, TwoDimensionalIntegerPoint point) {
+        
+    }
+}
--- src/main/resources/matlabcode/disk_b_test.m
+++ src/main/resources/matlabcode/disk_b_test.m
@@ -3,3 +3,4 @@ epsilon = [0.1:0.1:3];
 m1 = exp(epsilon)-1-epsilon;
 m2 = 1-exp(epsilon)+epsilon.*exp(epsilon);
 b=(2*m2+sqrt(4*m2.*m2+2*exp(epsilon).*m1.*m2))./(2*exp(epsilon).*m1);
+plot(epsilon,b);
